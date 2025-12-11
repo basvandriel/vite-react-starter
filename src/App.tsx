96ac0ed791832464a7f7180a06e0d5976bf7f969
Bas <contact@basvandriel.nl>
2025-12-11 17:47:54 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
